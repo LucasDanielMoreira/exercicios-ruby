03b2d5552ecf3f79fad3b3206f706e1ad71174d7
Lucas Daniel Moreira <lucasdanmoreira@gmail.com>
2023-01-31 18:04:02 +0000

    EXERCICIO SALARIO

--- 01-operacoes-aritmeticas/salario.rb
+++ 01-operacoes-aritmeticas/salario.rb
@@ -1,9 +1,9 @@
 print "Informe o salário: "
 salario = gets.chomp.to_f
 print "Informe o reajuste: "
-reajuste = gets.chop.to_f
+reajuste = gets.chop.to_i
 
-percentagem_salario = (reajuste / 100) * salario
+percentagem_salario = (reajuste.to_f / 100) * salario
 novo_salario = percentagem_salario + salario
 
 print "\nNovo salário: #{novo_salario}"
