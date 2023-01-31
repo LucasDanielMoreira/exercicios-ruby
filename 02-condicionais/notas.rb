330f5ebfb4aa454b0e3107400c604d6e1c846965
Lucas Daniel Moreira <lucasdanmoreira@gmail.com>
2023-01-31 23:58:25 +0000

    ALTERACAO NOTAS

--- 02-condicionais/notas.rb
+++ 02-condicionais/notas.rb
@@ -1,10 +1,10 @@
 print "Informe a primeira nota: "
-nota1 = gets.chomp.to_f
+note1 = gets.chomp.to_f
 print "Informe a segunda nota: "
-nota2 = gets.chomp.to_f
+note2 = gets.chomp.to_f
 
-nota_final = nota1 + nota2
+final_note = note1 + note2
 
-puts "\nNota final: #{nota_final.round(1)}"
+puts "\nNota final: #{final_note.round(1)}"
 
-puts "REPROVADO" if nota_final < 60
+puts "REPROVADO" if final_note < 60
