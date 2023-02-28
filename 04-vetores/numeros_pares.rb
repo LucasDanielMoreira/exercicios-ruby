30a81c89cd212aace7fc00c1812052c0b795498a
Lucas Daniel Moreira <lucasdanmoreira@gmail.com>
2023-02-28 20:57:28 +0000

    EXERCICIO NUMEROS PARES

---
+++ 04-vetores/numeros_pares.rb
@@ -0,0 +1,23 @@
+require_relative '../numbers_quantity'
+
+numbers_quantity
+
+puts
+
+@numbers = []
+
+@even = []
+
+def gets_numbers
+  @n.times do |i|
+    print "Digite o #{i + 1}º número: "
+    @numbers[i] = gets.chomp.to_i
+
+    @even << @numbers[i] if @numbers[i].even?
+  end
+end
+
+gets_numbers
+
+puts "NÚMEROS PARES:\n#{@even.join(' ')}"
+puts "\nQUANTIDADE DE PARES = #{@even.count}"
