b62e048c73044f92f581c118895c399f23a3c445
Lucas Daniel Moreira <lucasdanmoreira@gmail.com>
2023-02-28 19:10:05 +0000

    EXERCICIO NEGATIVOS

--- 04-vetores/negativos.rb
+++ 04-vetores/negativos.rb
@@ -1,22 +1,27 @@
-print "Quantos números serão informados? "
-n = gets.chomp.to_i
+require_relative '../numbers_quantity.rb'
 
-if n < 0
-  print "VALOR INVÀLIDO! \nTENTE NOVAMENTE!\n"
-  n = gets.chomp.to_i
+numbers_quantity
+
+if @n < 0
+  print "VALOR INVÁLIDO! \nTENTE NOVAMENTE!\n"
+  @n = gets.chomp.to_i
 end
 
-print "\n"
+puts
 
-numbers = Array.new
+@numbers = []
 
-n.times do |i|
-  print "Digite o #{i + 1}º número: "
-  numbers[i] = gets.chomp.to_i
+def gets_numbers
+  @n.times do |i|
+    print "Digite o #{i + 1}º número: "
+    @numbers[i] = gets.chomp.to_i
+  end
 end
+
+gets_numbers
 
 puts "\nNÚMEROS NEGATIVOS:"
 
-numbers.each do |number|
-  puts number if number < 0
+@numbers.each do |number|
+  puts number if number.negative?
 end
