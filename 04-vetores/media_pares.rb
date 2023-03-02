6cc320bd38fef4e3630eabd9be1c74268306f59b
Lucas Daniel Moreira <lucasdanmoreira@gmail.com>
2023-03-02 23:00:59 +0000

    EXERCICIO MEDIA PARES

--- 04-vetores/media_pares.rb
+++ 04-vetores/media_pares.rb
@@ -1,4 +1,5 @@
 require_relative '../numbers_quantity'
+require_relative '../gets_numbers'
 
 numbers_quantity('Quantos números vai ter no vetor? ')
 
@@ -8,16 +9,11 @@ puts
 
 @even = []
 
-def gets_numbers
-  @n.times do |i|
-    print "Digite o #{i + 1}º número: "
-    @numbers[i] = gets.chomp.to_i
+gets_numbers(@n, :to_i)
 
-    @even << @numbers[i] if @numbers[i].even?
-  end
+@numbers.each do |number|
+  @even << number if number.even?
 end
-
-gets_numbers
 
 if @even.count > 0
   puts "\nMÉDIA DOS PARES: #{(@even.sum / @even.count).round(1)}"
